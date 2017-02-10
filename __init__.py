4870cdd7cd957a9d3a7667b3feea1d379480022c
Christopher Bruns <cmbruns@rotatingpenguin.com>
2017-02-10 16:40:14 -0500

    Implement drawing a sphere at each node.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+# Plugin for loading SWC format neuron structures into Blender
 
 bl_info = {
     'name': 'Load SWC neuron',
@@ -36,16 +37,21 @@ class SwcNeuronImporter(bpy.types.Operator, ImportHelper):
         return os.path.splitext(os.path.basename(self.filepath))[0]
     
     def execute(self, context):
+        "Load the SWC file the user already specified, and display the geometry in the current scene"
         self.load_swc_file(self.filepath)
         swc_name = self._neuron_name()
         # print (swc_name)
+        # Create a top level container to hold the neuron representation(s)
         bpy.ops.object.empty_add()
         root = bpy.context.object
         root.name = swc_name
+        # Create a neuron skeleton mesh using only vertices and edges (ignoring vertex radii)
         skeleton = self.create_skeleton()
         skeleton.parent = root
+        # Create a sphere at each neuron vertex
         spheres = self.create_node_spheres()
         spheres.parent = root
+        # TODO: Create a truncated cone at each edge
         return {'FINISHED'}
         
     def load_swc_file(self, filename):
@@ -89,31 +95,38 @@ class SwcNeuronImporter(bpy.types.Operator, ImportHelper):
         return self.nodes
     
     def create_node_spheres(self):
-        # Put all the spheres into one parent object
-        bpy.ops.object.empty_add()
-        spheres = bpy.context.object
-        spheres.name = self._neuron_name() + ' node spheres'
-        # Create one sphere, which will be linked and scaled at each node
-        # bpy.ops.surface.primitive_nurbs_surface_sphere_add(
-        #     radius=1.0,
-        #     location=(0,0,0))
-        # sphere = bpy.context.object
-        for id, v in self.nodes.items():
-            r = v['radius']
-            xyz = v['xyz']
-            # Reuse the prototype sphere data
-            bpy.ops.surface.primitive_nurbs_surface_sphere_add(
-                radius=1,
-                location=(0,0,0))
-            placed_sphere = bpy.context.object
-            # placed_sphere = bpy.data.objects.new("Neuron Node", sphere.data)
-            placed_sphere.scale = (r,r,r)
-            placed_sphere.location = xyz
-            placed_sphere.parent = spheres
-        # Remove the prototype from the scene
-        # bpy.ops.object.select_all(action='DESELECT')
-        # bpy.context.scene.objects.unlink(sphere)
-        return spheres
+        "Place one sphere at each vertex of the neuron structure"
+        # Create a single shared material for the neuron
+        material = bpy.data.materials.new('Mat_spheres_' + self._neuron_name())
+        swc_mesh = bpy.data.meshes.new('Mesh_spheres_' + self._neuron_name())
+        # Create a face of the appropriate size at each node, so we can use dupli-faces
+        verts = []
+        faces = []
+        for id, data in self.nodes.items():
+            i = len(verts)
+            x,y,z = data['xyz']
+            r = 0.5 * data['radius']
+            # Four corners of square face
+            verts.append([x-r, y-r, z])
+            verts.append([x-r, y+r, z])
+            verts.append([x+r, y+r, z])
+            verts.append([x+r, y-r, z])
+            faces.append([i, i+1, i+2, i+3])
+        swc_mesh.from_pydata(verts, [], faces)
+        swc_mesh.update()
+        swc_obj = bpy.data.objects.new('Spheres_' + self._neuron_name(), swc_mesh)
+        bpy.context.scene.objects.link(swc_obj)
+        # One nurbs sphere as a prototype
+        bpy.ops.surface.primitive_nurbs_surface_sphere_add(
+                    view_align=False, enter_editmode=False,
+                    location=(0,0,0), rotation=(0.0, 0.0, 0.0),
+                    radius=1.0)
+        ball = bpy.context.scene.objects.active
+        ball.active_material = material
+        ball.parent = swc_obj
+        swc_obj.dupli_type = 'FACES'
+        swc_obj.use_dupli_faces_scale = True
+        return swc_obj
     
     def create_skeleton(self):
         "Skeletal model of neuron, using only mesh vertices and edges"
